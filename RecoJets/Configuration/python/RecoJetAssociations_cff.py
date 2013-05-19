66de568dc84400ac5f0e354d501fa1ec00d552d4
Giulio Eulisse <giulio.eulisse@cern.ch>
2013-05-19 14:06:28 +0200

    CMSSW_6_2_X_2013-05-19-0200

--- RecoJets/Configuration/python/RecoJetAssociations_cff.py
+++ RecoJets/Configuration/python/RecoJetAssociations_cff.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-# $Id: RecoJetAssociations_cff.py,v 1.7 2012/07/11 14:11:59 srappocc Exp $
+# $Id: RecoJetAssociations_cff.py,v 1.9 2013/05/01 20:46:01 srappocc Exp $
 #
 # Associate reconstructed jets with other objects
 # keep IC5 association for backward compatibility for external use
@@ -12,6 +12,5 @@ from RecoJets.JetAssociationProducers.sisCone5JTA_cff import *
 from RecoJets.JetAssociationProducers.kt4JTA_cff import *
 from RecoJets.JetAssociationProducers.ak5JTA_cff import *
 from RecoJets.JetAssociationProducers.ak7JTA_cff import *
-recoJetAssociations = cms.Sequence(ic5JetTracksAssociatorAtVertex+iterativeCone5JTA+kt4JTA+ak5JTA+ak7JTA)
-
+recoJetAssociations = cms.Sequence(ic5JetTracksAssociatorAtVertex+iterativeCone5JTA+kt4JTA+ak5JTA)
 recoJetAssociationsExplicit = cms.Sequence(ak5JTAExplicit)
